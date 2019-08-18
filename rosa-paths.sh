84abc247b3b9fa2c2c4f66832f2534472eed00cf
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-08-18 19:41:03 +0300

    Remove dead code from rosa-paths.sh

--- rosa-paths.sh
+++ rosa-paths.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Recommended to run from root for
-# better performance of urpmf
-
 set -efu
 
 TMP="${TMP:-$(mktemp -d)}"
@@ -18,94 +15,6 @@ mk_paths_list(){
 		sed -e 's,/(,/,g' -e 's,(/,/,g' -e 's,\\+,+,g' -e 's,\\$,,g' | \
 		sort -u > "$PL"
 	echo "PL: $PL"
-}
-
-process_list(){
-	while read -r line
-	do
-		# If this path exists...
-		if urpmf "$line" 2>/dev/null >/dev/null; then
-			if [ ! -f "$NOCHANGE_LIST" ]; then
-				touch "$NOCHANGE_LIST"
-			fi
-			if ! grep -q "^$line" "$NOCHANGE_LIST"; then
-				echo "$line" >> "$NOCHANGE_LIST"
-			fi
-			continue
-		fi
-		if echo "$line" | grep -qE '^/usr/bin/|^/usr/sbin/|^/bin/|^/sbin/'; then
-			bin="$(echo "$line" | awk -F 'bin/' '{print $NF}')"
-			echo "$bin"
-			urpmf_results="$(urpmf "$bin" | awk -F ':' '{print $NF}' | grep "bin/")"
-			unset append
-			if echo "$urpmf_results" | grep -q "${bin}$"
-				then
-					append="$(echo "$urpmf_results" | grep -q "${bin}$")"
-					echo "${line}:::${append}"
-			fi
-			#while read -r u_line
-			#do
-			#	if echo "$u_line" | grep -q '^/usr/'
-			#		then awkN=3
-			#		else awkN=2
-			#	fi
-			#done < <(echo "$urpmf_results")
-		fi
-	done < <(grep -v '/$' "$PL")
-}
-
-_duplicate(){
-	while [ -n "$1" ]
-	do
-		case "$1" in
-			--file ) shift; FILE="$1" ;;
-			--path ) shift; PATH="$1" ;;
-			--line-number ) shift; LINE_NUMBER_ORIG="$1" ;;
-			--original-line ) shift; LINE_ORIG="$1" ;;
-		esac
-		shift
-	done
-	
-	if [ -z "$FILE" ] || [ -z "$PATH" ] || [ -z "$LINE_NUMBER_ORIG" ] || [ -z "$LINE_ORIG" ]; then
-		echo "Some variables are empty"
-		return 1
-	fi
-	
-	#LINE_NEW="$(echo "$LINE_ORIG" | sed -e )"
-}
-
-dublicate_paths(){
-	if [ ! -f "$1" ]; then
-		echo "File $file not found"
-		return 1
-	fi
-	file="$1"
-	
-	# number of line in file that we are working with
-	lnum=0
-	while read -r line
-	do
-		if ! echo "$line" | grep -q '^/'; then
-			# this line is not a path
-			continue
-		fi
-		
-		for bin in '/usr/bin' '/usr/sbin' '/bin' '/sbin'
-		do
-			if echo "$line" | grep -qE "^$bin"; then
-				_duplicate \
-					--file "$file" \
-					--path "$line" \
-					--line-number "$lnum" \
-					--original-line="$line"
-				# as we appended new line in place
-				lnum=$((lnum+1))
-				continue
-			fi
-		done
-		
-		lnum=$((lnum+1))
-	done < "$file"
 }
 
 copy_and_add_paths(){
@@ -180,5 +89,4 @@ process_dublicate_paths(){
 }
 
 mk_paths_list
-#process_list
 process_dublicate_paths
