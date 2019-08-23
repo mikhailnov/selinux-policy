43579efbd9473dad477a41019d1c86da6e96c6e0
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-08-24 01:13:20 +0300

    Avoid duplicating entries via rosa-paths.sh

--- rosa-paths.sh
+++ rosa-paths.sh
@@ -17,6 +17,22 @@ mk_paths_list(){
 	echo "PL: $PL"
 }
 
+# append with out suplicates -- and
+_and(){
+	# Usage: _and <file> <string> <sed pattern>
+	if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+		echo "Not sufficient args, usage: _and <file> <string> <sed pattern>"
+		return 1
+	fi
+	line_fixed="$(echo "$2" | sed -e 's,  , ,g' -e 's,\t, ,g')"
+	if grep -q "$2" "$1" || grep -q "$line_fixed" "$1"
+		then
+			:
+		else
+			echo "$2" | sed -e "$3" >> "$1"
+	fi
+}
+
 copy_and_add_paths(){
 	if [ ! -f "$1" ]; then
 		echo "File $file not found"
@@ -32,48 +48,48 @@ copy_and_add_paths(){
 		echo "$line" >> "$new_file"
 		
 		if echo "$line" | grep -q '^/bin'; then
-			echo "$line" | sed -e 's,^/bin,/usr/bin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/bin,/sbin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/bin,/usr/sbin,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/bin,/usr/bin,g'
+			_and "$new_file" "$line" 's,^/bin,/sbin,g'
+			_and "$new_file" "$line" 's,^/bin,/usr/sbin,g'
 			continue
 		fi
 		if echo "$line" | grep -q '^/sbin'; then
-			echo "$line" | sed -e 's,^/sbin,/usr/sbin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/sbin,/bin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/sbin,/usr/bin,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/sbin,/usr/sbin,g'
+			_and "$new_file" "$line" 's,^/sbin,/bin,g'
+			_and "$new_file" "$line" 's,^/sbin,/usr/bin,g'
 			continue
 		fi
 		if echo "$line" | grep -q '^/usr/bin'; then
-			echo "$line" | sed -e 's,^/usr/bin,/usr/sbin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/bin,/bin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/bin,/sbin,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/usr/bin,/usr/sbin,g'
+			_and "$new_file" "$line" 's,^/usr/bin,/bin,g'
+			_and "$new_file" "$line" 's,^/usr/bin,/sbin,g'
 			continue
 		fi
 		if echo "$line" | grep -q '^/usr/sbin'; then
-			echo "$line" | sed -e 's,^/usr/sbin,/usr/bin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/sbin,/bin,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/sbin,/sbin,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/usr/sbin,/usr/bin,g'
+			_and "$new_file" "$line" 's,^/usr/sbin,/bin,g'
+			_and "$new_file" "$line" 's,^/usr/sbin,/sbin,g'
 			continue
 		fi
 
 		if echo "$line" | grep -q '^/usr/lib/systemd'; then
-			echo "$line" | sed -e 's,^/usr/lib/systemd,/lib/systemd,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/lib/systemd,/usr/share/systemd,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/usr/lib/systemd,/lib/systemd,g'
+			_and "$new_file" "$line" 's,^/usr/lib/systemd,/usr/share/systemd,g'
 			continue
 		fi
 
 		if echo "$line" | grep -q '^/usr/lib/'; then
-			echo "$line" | sed -e 's,^/usr/lib/,/lib/,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/lib/,/lib64/,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/lib/,/usr/lib64/,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/lib/,/usr/libexec/,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/usr/lib/,/lib/,g'
+			_and "$new_file" "$line" 's,^/usr/lib/,/lib64/,g'
+			_and "$new_file" "$line" 's,^/usr/lib/,/usr/lib64/,g'
+			_and "$new_file" "$line" 's,^/usr/lib/,/usr/libexec/,g'
 			continue
 		fi
 		if echo "$line" | grep -q '^/usr/libexec'; then
-			echo "$line" | sed -e 's,^/usr/libexec,/lib,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/libexec,/lib64,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/libexec,/usr/lib,g' >> "$new_file"
-			echo "$line" | sed -e 's,^/usr/libexec,/usr/lib64,g' >> "$new_file"
+			_and "$new_file" "$line" 's,^/usr/libexec,/lib,g'
+			_and "$new_file" "$line" 's,^/usr/libexec,/lib64,g'
+			_and "$new_file" "$line" 's,^/usr/libexec,/usr/lib,g'
+			_and "$new_file" "$line" 's,^/usr/libexec,/usr/lib64,g'
 			continue
 		fi
 		
